53ef6db62e3cba80362348b44881a83e37cf543a
Michael Hart <michael@mhart.dev>
2020-01-16 17:17:59 -0500

    MVP + 1 stretch goal (styling to distinguish between coins sent and received

--- basic_wallet_p/wallet/src/components/Ledger.jsx
+++ basic_wallet_p/wallet/src/components/Ledger.jsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import Moment from 'react-moment';
 
-const Ledger = (props) => {
+import './Ledger.scss';
+
+const Ledger = ({ transactions }) => {
 	const { user } = useParams();
-	return <div className='Ledger'>Ledger for {user}</div>;
+	const userTransactions = transactions[user] || [];
+	const [ balance, setBalance ] = useState();
+
+	useEffect(
+		() => {
+			setBalance(
+				userTransactions.reduce((total, { amount, recipient }) => {
+					return (total += recipient === user ? amount : 0 - amount);
+				}, 0)
+			);
+		},
+		[ user, userTransactions ]
+	);
+
+	console.log(balance);
+
+	return (
+		<div className='Ledger'>
+			{userTransactions.length ? (
+				<div>
+					<h1>Ledger for {user}</h1>
+					<h2>Balance: {balance} LC</h2>
+					<div className='transactions'>
+						{userTransactions.reverse().map(({ amount, recipient, sender, timestamp }) => {
+							const direction = recipient === user ? 'receiver' : 'sender';
+							return (
+								<div className={`line-item ${direction}`}>
+									<div className='field'>
+										<Moment fromNow>{timestamp * 1000}</Moment>
+									</div>
+									<div className='field'>Sender: {sender}</div>
+									<div className='field'>Recipient: {recipient}</div>
+									<div className='field'>Amount: {amount}</div>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			) : (
+				<div>No transactions found</div>
+			)}
+		</div>
+	);
 };
 
 export default Ledger;
